42499dd852f9cbae84f17a64ccd10230f017f594
Benjamin Gillott <bg357@cornell.edu>
2020-06-17 22:27:08 -0400

    ehhh kinda giving up on pause

--- assignment1/states/PauseState.lua
+++ assignment1/states/PauseState.lua
@@ -12,18 +12,39 @@ PauseState = Class{__includes = BaseState}
 
 --
 
-function PauseState:enter()
-    --add sth about fancy bird here
+function PauseState:init(bird, pipePairs, timer, score)
+    self.bird = Bird()
+    self.pipePairs = {}
+
+    self.bird = bird
+    self.pipePairs = pipePairs
+    self.timer = timer
+    self.score = score
 end
 
 function PauseState:update(dt)
     -- unpause if p is pressed
     if love.keyboard.wasPressed('p') then
-        gStateMachine:change('play')
+        gStateMachine:change('play', {
+            bird = self.bird,
+            pipePairs = self.pipePairs,
+            timer = self.timer,
+            score = self.score
+        })
     end
 end
 
 function PauseState:render()
+
+    -- for k, pair in pairs(self.pipePairs) do
+    --     pair:render()
+    -- end
+
+    love.graphics.setFont(flappyFont)
+    love.graphics.print('Score: ' .. tostring(self.score), 8, 8)
+
+    -- self.bird:render()
+
     -- simply render the score to the middle of the screen
     love.graphics.setFont(flappyFont)
     love.graphics.printf('Game Is Paused', 0, 64, VIRTUAL_WIDTH, 'center')
